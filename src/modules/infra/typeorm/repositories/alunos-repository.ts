c50fe57e38b7c3d8fe4fbc552afcb6919a301bf8
Schmxdt <airtonschmidt2018@gmail.com>
2025-05-14 14:09:34 -0300

    feat: Crud alunos repository

--- src/modules/infra/typeorm/repositories/alunos-repository.ts
+++ src/modules/infra/typeorm/repositories/alunos-repository.ts
@@ -1,7 +1,7 @@
 import { Repository } from 'typeorm';
 import { AppDataSource } from '@shared/infra/data-source';
 import { serverError } from '@shared/helpers/http';
-import { IAlunosRepository } from '@modules/repositories/i-aluno-repository';
+import { IAlunosRepository } from '@modules/repositories/i-alunos-repository';
 import { Alunos } from '../entities/alunos';
 import { IAlunoDTO } from '@modules/dtos/i-aluno-dto';
 import { v4 as uuidv4 } from 'uuid';
@@ -38,117 +38,110 @@ export class AlunosRepository implements IAlunosRepository {
   }
 
   // // List
-  // async list(
-  //   search: string,
-  //   page: number,
-  //   rowsPerPage: number,
-  //   columnOrder: Array<'ASC' | 'DESC'>
-  // ): Promise<Lms[]> {
-  //   try {
-  //     if (typeof columnOrder === 'undefined' || columnOrder.length === 0) {
-  //       columnOrder = ['ASC', 'ASC'];
-  //     }
+  async list(
+    search: string,
+    page: number,
+    rowsPerPage: number,
+    columnOrder: Array<'ASC' | 'DESC'>
+  ): Promise<Alunos[]> {
+    try {
+      if (typeof columnOrder === 'undefined' || columnOrder.length === 0) {
+        columnOrder = ['ASC', 'ASC'];
+      }
 
-  //     const offset = rowsPerPage * page;
+      const offset = rowsPerPage * page;
 
-  //     let lms = await this.repository.createQueryBuilder('lms')
-  //       .select([
-  //         'lms.id as "id"',
-  //         'lms.name as "name"',
-  //         'lms.score as "score"',
-  //       ])
-  //       .andWhere('CAST(lms.name AS VARCHAR) ilike :search', { search: `%${search}%` })
-  //       .addOrderBy('lms.score', 'DESC')
-  //       .addOrderBy('lms.updated_at', 'ASC')
-  //       .addOrderBy('lms.name', columnOrder[0])
-  //       .offset(offset)
-  //       .limit(rowsPerPage)
-  //       .take(rowsPerPage)
-  //       .getRawMany();
+      let result = await this.repository.createQueryBuilder('alunos')
+        .select([
+          'alunos.id as "id"',
+          'alunos.nome as "nome"',
+          'alunos.email as "email"',
+          'alunos.data_nascimento as "data_nascimento"',
+        ])
+        .andWhere('CONVERT(alunos.nome USING utf8) LIKE :search', { search: `%${search}%` })
+        .addOrderBy('alunos.nome', 'DESC')
+        .addOrderBy('alunos.updated_at', 'ASC')
+        .addOrderBy('alunos.nome', columnOrder[0])
+        .offset(offset)
+        .limit(rowsPerPage)
+        .take(rowsPerPage)
+        .getRawMany();
 
-  //     return lms;
-  //   } catch (err) {
-  //     throw serverError(err);
-  //   }
-  // }
+      return result;
+    } catch (err) {
+      throw serverError(err);
+    }
+  }
 
   // // Get
-  // async get(id: string): Promise<Lms> {
-  //   try {
-  //     const lms = await this.repository.findOne({
-  //       where: { id },
-  //     });
+  async get(id: string): Promise<Alunos> {
+    try {
+      const result = await this.repository.findOne({
+        where: { id },
+      });
 
-  //     if (!lms) {
-  //       throw new Error('LMS not found');
-  //     }
+      if (!result) {
+        throw new Error('Alunos not found');
+      }
 
-  //     return lms;
-  //   } catch (err) {
-  //     throw serverError(err);
-  //   }
-  // }
+      return result;
+    } catch (err) {
+      throw serverError(err);
+    }
+  }
 
   // // Update
-  // async update({
-  //   id,
-  //   name,
-  //   ans1,
-  //   ans2,
-  //   ans3,
-  //   ans4,
-  //   ans5,
-  // }: ILmsDTO): Promise<Lms> {
-  //   try {
-  //     const lms = await this.repository.findOne({ where: { id } });
+  async update({
+    id,
+    nome,
+    email,
+    data_nascimento,
+  }: IAlunoDTO): Promise<Alunos> {
+    try {
+      const result = await this.repository.findOne({ where: { id } });
 
-  //     if (!lms) {
-  //       throw new Error('LMS not found');
-  //     }
+      if (!result) {
+        throw new Error('Alunos not found');
+      }
 
-  //     const score = await this.scoreCalculation(ans1, ans2, ans3, ans4, ans5);
-  //     lms.name = name;
-  //     lms.ans1 = ans1;
-  //     lms.ans2 = ans2;
-  //     lms.ans3 = ans3;
-  //     lms.ans4 = ans4;
-  //     lms.ans5 = ans5;
-  //     lms.score = score;
+      result.nome = nome;
+      result.email = email;
+      result.data_nascimento = data_nascimento;
 
-  //     await this.repository.save(lms);
+      await this.repository.save(result);
 
-  //     return lms;
-  //   } catch (err) {
-  //     throw serverError(err);
-  //   }
-  // }
+      return result;
+    } catch (err) {
+      throw serverError(err);
+    }
+  }
 
   // // Delete
-  // async delete(id: string): Promise<void> {
-  //   try {
-  //     const lms = await this.repository.findOne({ where: { id } });
+  async delete(id: string): Promise<void> {
+    try {
+      const result = await this.repository.findOne({ where: { id } });
 
-  //     if (!lms) {
-  //       throw new Error('LMS not found');
-  //     }
+      if (!result) {
+        throw new Error('Alunos not found');
+      }
 
-  //     await this.repository.remove(lms);
-  //   } catch (err) {
-  //     throw serverError(err);
-  //   }
-  // }
+      await this.repository.remove(result);
+    } catch (err) {
+      throw serverError(err);
+    }
+  }
 
   // // Count
-  // async count(search: string): Promise<{ count: number }> {
-  //   try {
-  //     const count = await this.repository.createQueryBuilder('lms')
-  //       .select('COUNT(*)', 'count')
-  //       .where('CAST(lms.name AS VARCHAR) ilike :search', { search: `%${search}%` })
-  //       .getRawOne();
+  async count(search: string): Promise<{ count: number }> {
+    try {
+      const count = await this.repository.createQueryBuilder('alunos')
+        .select('COUNT(*)', 'count')
+        .where('CONVERT(alunos.nome USING utf8) LIKE :search', { search: `%${search}%` })
+        .getRawOne();
 
-  //     return count;
-  //   } catch (err) {
-  //     throw serverError(err);
-  //   }
-  // }
+      return count;
+    } catch (err) {
+      throw serverError(err);
+    }
+  }
 }
